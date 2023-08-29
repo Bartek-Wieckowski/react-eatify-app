dfe5c631c8d6903e55931428e5f8e6b006cb4160
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-29 10:26:15 +0200

    used component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,12 @@
 import Logo from "../logo/Logo";
+import Search from "../search/Search";
 import "./header.scss";
 
 const Header = () => {
   return (
     <header className="header">
       <Logo />
+      <Search/>
     </header>
   );
 };
