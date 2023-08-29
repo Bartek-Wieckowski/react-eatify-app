a29bdccb0e351adee0d0a52c2885f27b023e1e1c
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-29 13:49:28 +0200

    added img user

--- src/utils/images.js
+++ src/utils/images.js
@@ -1,8 +1,10 @@
 import logo from "../assets/img/fork-knife-bold.svg";
 import searchSvg from "../assets/img/magnifying-glass-bold.svg";
+import user from "../assets/img/user-bold.svg";
 const imagePaths = {
   logo,
   searchSvg,
+  user,
 };
 
 export default imagePaths;
