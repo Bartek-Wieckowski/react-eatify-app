b1a6064881d38931bd35af6379918113bedc2498
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-29 14:43:24 +0200

    added spinner img

--- src/utils/images.js
+++ src/utils/images.js
@@ -1,10 +1,12 @@
 import logo from "../assets/img/fork-knife-bold.svg";
 import searchSvg from "../assets/img/magnifying-glass-bold.svg";
 import user from "../assets/img/user-bold.svg";
+import spinner from "../assets/img/circle-notch-bold.svg";
 const imagePaths = {
   logo,
   searchSvg,
   user,
+  spinner,
 };
 
 export default imagePaths;
