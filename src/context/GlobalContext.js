9df59d1523b4cae6e43fc3c2cafb2caddcd9fcfd
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-29 13:51:46 +0200

    created GlobalContext,reducer, Provider nad useGlobal function

---
+++ src/context/GlobalContext.js
@@ -0,0 +1,43 @@
+import { createContext, useContext, useReducer } from "react";
+
+const GlobalContext = createContext();
+
+const initialState = {
+  isLoading: false,
+  recipes: [],
+  error: "",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "loading":
+      return { ...state, isLoading: true };
+    case "recipes/loaded":
+      return { ...state, isLoading: false, recipes: action.payload };
+    case "rejected":
+      return { ...state, isLoading: false };
+
+    default:
+      throw new Error("Unknown action type");
+  }
+}
+
+function GlobalProvider({ children }) {
+  const [{ isLoading, recipes, error }, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <GlobalContext.Provider value={{ isLoading, recipes, error, dispatch }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+}
+
+function useGlobal() {
+  const context = useContext(GlobalContext);
+  if (context === undefined) {
+    throw new Error("GlobalContext was used outside the GlobalProvider");
+  }
+  return context;
+}
+
+export { useGlobal, GlobalProvider };
