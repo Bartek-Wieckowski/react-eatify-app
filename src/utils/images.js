fe07772096e937d6674c91229a39970f8fdb3a22
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-29 10:25:39 +0200

    added search img

--- src/utils/images.js
+++ src/utils/images.js
@@ -1,6 +1,8 @@
 import logo from "../assets/img/fork-knife-bold.svg";
+import searchSvg from "../assets/img/magnifying-glass-bold.svg";
 const imagePaths = {
   logo,
+  searchSvg,
 };
 
 export default imagePaths;
