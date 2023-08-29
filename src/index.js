7f47ff424b87b837f07841e2f26a421ec67be69f
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-29 13:50:20 +0200

    used GlobalProvider

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./_variables.scss";
 import "./index.scss";
 import App from "./App";
+import { GlobalProvider } from "./context/GlobalContext";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
   </React.StrictMode>
 );
