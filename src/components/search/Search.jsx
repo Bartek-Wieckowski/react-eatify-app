46e9b0e2aae44578e039fba46ec838e227fa7d8a
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-29 13:53:00 +0200

    added query state and inside handleSubmit fetch data from API save in recipes array from GlobalContext

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,11 +1,38 @@
+import { useState } from "react";
 import imagePaths from "../../utils/images";
 import "./search.scss";
+import { useGlobal } from "../../context/GlobalContext";
 
 const Search = () => {
+  const [query, setQuery] = useState("");
+  const { dispatch } = useGlobal();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const fetchRecipes = async () => {
+      try {
+        const res = await fetch(
+          `https://forkify-api.herokuapp.com/api/v2/recipes?search=${query}&key=` +
+            process.env.REACT_APP_KEY_API
+        );
+        const data = await res.json();
+        dispatch({ type: "recipes/loaded", payload: data.data.recipes });
+        console.log(data.data.recipes);
+      } catch (error) {}
+    };
+    fetchRecipes();
+  }
+
   return (
-    <form class="search">
-      <input type="text" class="search__field" placeholder="Search recipes..." />
-      <button class="btn search__btn">
+    <form className="search" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="search__field"
+        placeholder="Search recipes..."
+        onChange={(e) => setQuery(e.target.value)}
+        value={query}
+      />
+      <button className="btn search__btn" type="submit">
         <img src={imagePaths.searchSvg} alt="" />
       </button>
     </form>
