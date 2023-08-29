9fda675e5099e69ca35fdfa7dc8dc6e77743aba6
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-29 15:21:38 +0200

    added warning img

--- src/utils/images.js
+++ src/utils/images.js
@@ -2,11 +2,13 @@ import logo from "../assets/img/fork-knife-bold.svg";
 import searchSvg from "../assets/img/magnifying-glass-bold.svg";
 import user from "../assets/img/user-bold.svg";
 import spinner from "../assets/img/circle-notch-bold.svg";
+import warning from "../assets/img/warning-bold.svg";
 const imagePaths = {
   logo,
   searchSvg,
   user,
   spinner,
+  warning,
 };
 
 export default imagePaths;
