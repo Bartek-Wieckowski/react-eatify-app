5a4f55b49653aa010c96b846510884bc4fe11d7b
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-29 13:51:05 +0200

    created List component and map recipe array inside

---
+++ src/components/list/List.jsx
@@ -0,0 +1,31 @@
+import { useGlobal } from "../../context/GlobalContext";
+import imagePaths from "../../utils/images";
+import "./list.scss";
+
+const List = () => {
+  const { recipes } = useGlobal();
+  return (
+    <div className="search-results">
+      <ul className="results">
+        {recipes?.map((recipe) => (
+          <li className="preview" key={recipe.id}>
+            <a className="preview__link preview__link--active" href="#23456">
+              <figure className="preview__fig">
+                <img src={recipe.image_url} alt={recipe.title} />
+              </figure>
+              <div className="preview__data">
+                <h4 className="preview__title">{recipe.title}</h4>
+                <p className="preview__publisher">{recipe.publisher}</p>
+                <div className="preview__user-generated">
+                    <img src={imagePaths.user} />
+                </div>
+              </div>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default List;
