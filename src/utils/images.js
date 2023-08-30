fc53da020ff2bfd3e063e8179823c5f098d372ea
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-30 15:16:51 +0200

    added arrow images

--- src/utils/images.js
+++ src/utils/images.js
@@ -3,12 +3,16 @@ import searchSvg from "../assets/img/magnifying-glass-bold.svg";
 import user from "../assets/img/user-bold.svg";
 import spinner from "../assets/img/circle-notch-bold.svg";
 import warning from "../assets/img/warning-bold.svg";
+import arrowRight from "../assets/img/arrow-right-bold.svg";
+import arrowLeft from "../assets/img/arrow-left-bold.svg";
 const imagePaths = {
   logo,
   searchSvg,
   user,
   spinner,
   warning,
+  arrowRight,
+  arrowLeft,
 };
 
 export default imagePaths;
