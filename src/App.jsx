5ee0d084958432594b81cbdbcbbc395c12fbb0a4
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-30 16:11:09 +0200

    added RecipeDetails and used BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { BrowserRouter } from "react-router-dom";
 import Header from "./components/header/Header";
 import List from "./components/list/List";
+import RecipeDetails from "./pages/RecipeDetails";
 
 function App() {
   return (
     <div className="container">
-      <Header />
-      <List />
+      <BrowserRouter>
+        <Header />
+        <List />
+        <RecipeDetails />
+      </BrowserRouter>
     </div>
   );
 }
