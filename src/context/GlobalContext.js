a0f50df414baf42e6d544fa0abb491c4163aaa37
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-31 16:07:09 +0200

    added key in initialState, created function which fetch one recipe

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -1,11 +1,12 @@
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useCallback, useContext, useReducer } from "react";
 
 const GlobalContext = createContext();
 
 const initialState = {
   isLoading: false,
-  recipes: [],
   error: "",
+  recipes: [],
+  currentRecipe: {},
 };
 
 function reducer(state, action) {
@@ -14,6 +15,8 @@ function reducer(state, action) {
       return { ...state, isLoading: true };
     case "recipes/loaded":
       return { ...state, isLoading: false, recipes: action.payload, error: "" };
+    case "recipe/loaded":
+      return { ...state, isLoading: false, currentRecipe: action.payload, error: "" };
     case "rejected":
       return { ...state, isLoading: false, error: action.payload };
 
@@ -23,10 +26,29 @@ function reducer(state, action) {
 }
 
 function GlobalProvider({ children }) {
-  const [{ isLoading, recipes, error }, dispatch] = useReducer(reducer, initialState);
+  const [{ isLoading, recipes, error, currentRecipe }, dispatch] = useReducer(reducer, initialState);
+
+  const getRecipe = useCallback(
+    async function getRecipe(id) {
+      if (Number(id) === currentRecipe.id) return;
+      dispatch({ type: "loading" });
+
+      try {
+        const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
+        const data = await res.json();
+        dispatch({ type: "recipe/loaded", payload: data.data.recipe });
+      } catch (error) {
+        dispatch({
+          type: "reject",
+          payload: "There was an error loading the recipe...",
+        });
+      }
+    },
+    [currentRecipe.id]
+  );
 
   return (
-    <GlobalContext.Provider value={{ isLoading, recipes, error, dispatch }}>
+    <GlobalContext.Provider value={{ isLoading, recipes, error, currentRecipe, getRecipe, dispatch }}>
       {children}
     </GlobalContext.Provider>
   );
