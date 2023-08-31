1ac9bee8fc5e3cc53e52bfdd461a9e6ed001df07
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-31 16:08:34 +0200

    added css, used getRecipe from context and display object currentRecipe values

--- src/components/single-recipe/SingleRecipe.jsx
+++ src/components/single-recipe/SingleRecipe.jsx
@@ -1,9 +1,33 @@
 import { useParams } from "react-router-dom";
 import "./single-recipe.scss";
+import { useEffect } from "react";
+import { useGlobal } from "../../context/GlobalContext";
+import Spinner from "../spinner/Spinner";
 
 const SingleRecipe = () => {
   const { id } = useParams();
-  return <div style={{fontSize: "48px"}}>SingleRecipe = {id}</div>;
+  const { isLoading, getRecipe, currentRecipe } = useGlobal();
+
+  useEffect(() => {
+    getRecipe(id);
+  }, [id, getRecipe]);
+
+  const { title, image_url } = currentRecipe;
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  return (
+    <>
+      <figure className="recipe__fig">
+        <img src={image_url} alt="Tomato" className="recipe__img" />
+        <h1 className="recipe__title">
+          <span>{title}</span>
+        </h1>
+      </figure>
+    </>
+  );
 };
 
 export default SingleRecipe;
