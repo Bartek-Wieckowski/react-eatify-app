87c22c648cf36c040911a5727d368f236ef4109f
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-08-31 16:26:46 +0200

    added images icons

--- src/utils/images.js
+++ src/utils/images.js
@@ -5,6 +5,12 @@ import spinner from "../assets/img/circle-notch-bold.svg";
 import warning from "../assets/img/warning-bold.svg";
 import arrowRight from "../assets/img/arrow-right-bold.svg";
 import arrowLeft from "../assets/img/arrow-left-bold.svg";
+import plus from "../assets/img/plus-circle-bold.svg";
+import minus from "../assets/img/minus-circle-bold.svg";
+import users from "../assets/img/users-bold.svg";
+import clock from "../assets/img/clock-bold.svg";
+import bookmark from "../assets/img/bookmark-bold.svg";
+
 const imagePaths = {
   logo,
   searchSvg,
@@ -13,6 +19,11 @@ const imagePaths = {
   warning,
   arrowRight,
   arrowLeft,
+  plus,
+  minus,
+  users,
+  clock,
+  bookmark,
 };
 
 export default imagePaths;
