f9da553523b30afebcf5cf28da81a5c3a352a670
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-01 09:52:49 +0200

    used useEffect to fetch details recipe, used to display keys, and added logic is isLoading, error and navigate to homepage when query is not found

--- src/components/single-recipe/SingleRecipe.jsx
+++ src/components/single-recipe/SingleRecipe.jsx
@@ -1,21 +1,52 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import "./single-recipe.scss";
-import { useEffect } from "react";
-import { useGlobal } from "../../context/GlobalContext";
+import { useEffect, useState } from "react";
 import Spinner from "../spinner/Spinner";
+import ErrorMsg from "../error-msg/ErrorMsg";
+import imagePaths from "../../utils/images";
+import { useGlobal } from "../../context/GlobalContext";
 
 const SingleRecipe = () => {
   const { id } = useParams();
-  const { isLoading, getRecipe, currentRecipe } = useGlobal();
+  const [currentRecipe, setCurrentRecipe] = useState({} || null);
+  const [isLoadingLocal, setIsLoadingLocal] = useState(false);
+  const [errorLocal, setErrorLocal] = useState(false);
+  const { error: globalError } = useGlobal();
+  const navigate = useNavigate();
 
   useEffect(() => {
+    async function getRecipe(id) {
+      if (currentRecipe.id !== undefined && Number(id) === currentRecipe.id) return;
+      setIsLoadingLocal(true);
+
+      try {
+        const res = await fetch(
+          `https://forkify-api.herokuapp.com/api/v2/recipes/${id}?key` + process.env.REACT_APP_KEY_API
+        );
+        const data = await res.json();
+        setCurrentRecipe(data.data.recipe);
+      } catch (error) {
+        setErrorLocal(true);
+      } finally {
+        setIsLoadingLocal(false);
+      }
+    }
     getRecipe(id);
-  }, [id, getRecipe]);
+  }, [currentRecipe.id, id]);
+
+  useEffect(() => {
+    if (globalError) {
+      navigate("/");
+    }
+  }, [globalError, navigate]);
 
   const { title, image_url } = currentRecipe;
 
-  if (isLoading) {
+  if (isLoadingLocal) {
     return <Spinner />;
+  }
+  if (errorLocal) {
+    return <ErrorMsg>{"Something went wrong..."}</ErrorMsg>;
   }
 
   return (
@@ -26,6 +57,30 @@ const SingleRecipe = () => {
           <span>{title}</span>
         </h1>
       </figure>
+      <div className="recipe__details">
+        <div className="recipe__info">
+          <img src={imagePaths.clock} alt="" />
+          <span className="recipe__info-data recipe__info-data--minutes">45</span>
+          <span className="recipe__info-text">minutes</span>
+        </div>
+        <div className="recipe__info">
+          <img src={imagePaths.users} alt="" />
+          <span className="recipe__info-data recipe__info-data--people">4</span>
+          <span className="recipe__info-text">servings</span>
+
+          <div className="recipe__info-buttons">
+            <button className="btn--tiny btn--increase-servings">
+              <img src={imagePaths.minus} alt="" />
+            </button>
+            <button className="btn--tiny btn--increase-servings">
+              <img src={imagePaths.plus} alt="" />
+            </button>
+          </div>
+        </div>
+        <button className="btn--tiny">
+          <img src={imagePaths.bookmark} alt="" />
+        </button>
+      </div>
     </>
   );
 };
