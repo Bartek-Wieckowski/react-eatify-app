222320fd6f75650f6a321aaeb12941aaf7cb6dfa
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-04 16:00:04 +0200

    get items from localStorage and display in jsx, if localStorage empty show msg

--- src/components/bookmarks/Bookmarks.jsx
+++ src/components/bookmarks/Bookmarks.jsx
@@ -1,7 +1,16 @@
+import { useEffect, useState } from "react";
 import imagePaths from "../../utils/images";
 import "./bookmarks.scss";
+import { Link } from "react-router-dom";
 
 function Bookmarks() {
+  const [bookmarks, setBookmarks] = useState([]);
+
+  useEffect(() => {
+    const storedBookmarks = JSON.parse(localStorage.getItem("favRecipes")) || [];
+    setBookmarks(storedBookmarks);
+  }, []);
+
   return (
     <nav className="nav">
       <ul className="nav__list">
@@ -9,24 +18,28 @@ function Bookmarks() {
           <button className="nav__btn nav__btn--bookmarks">
             <img src={imagePaths.bookmark} alt="" /> Bookmarks
           </button>
-          <div class="bookmarks">
-            <ul class="bookmarks__list">
-              <div class="message">
-                <div></div>
-                <p>No bookmarks yet. Find a nice recipe and bookmark it :)</p>
-              </div>
+          <div className="bookmarks">
+            <ul className="bookmarks__list">
+              {bookmarks.length === 0 && (
+                <div className="message">
+                  <div></div>
+                  <p>No bookmarks yet. Find a nice recipe and bookmark it :)</p>
+                </div>
+              )}
 
-              <li class="preview">
-                <a class="preview__link" href="#23456">
-                  <figure class="preview__fig">
-                    <img src="src/img/test-1.jpg" alt="Test" />
-                  </figure>
-                  <div class="preview__data">
-                    <h4 class="preview__name">Pasta with Tomato Cream ...</h4>
-                    <p class="preview__author">The Pioneer Woman</p>
-                  </div>
-                </a>
-              </li>
+              {bookmarks.map((b) => (
+                <li className="preview" key={b.id}>
+                  <Link className="preview__link" to={"recipe/" + b.id}>
+                    <figure className="preview__fig">
+                      <img src={b.image_url} alt="Test" />
+                    </figure>
+                    <div className="preview__data">
+                      <h4 className="preview__name">{b.title}</h4>
+                      <p className="preview__author">{b.publisher}</p>
+                    </div>
+                  </Link>
+                </li>
+              ))}
             </ul>
           </div>
         </li>
